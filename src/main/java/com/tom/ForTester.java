b7b283b032b80d2ee30957529911afacf1aa8a53
Hank Tom <hanktom@gmail.com>
2021-07-14 20:30:21 +0800

    method and approve sign

--- src/main/java/com/tom/ForTester.java
+++ src/main/java/com/tom/ForTester.java
@@ -2,12 +2,11 @@ package com.tom;
 
 public class ForTester {
     public static void main(String[] args) {
-        int row = 10;
-        int col = 10;
-        /*
+        int row = 15;
+        int col = 30;
         for (int i=0 ; i<row; i++) {
             for (int j = 0; j < col; j++) {
-                if (i==j || j==col-1-i) {
+                if ((i==j && i>row/2) || j==col-1-i) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -15,9 +14,13 @@ public class ForTester {
             }
             System.out.println();
         }
-        */
-        row = 15;
-        col = 30;
+//        drawAnX();
+    }
+
+    private static void drawAnX() {
+        int row = 10;
+        int col = 10;
+        //draw an X
         for (int i=0 ; i<row; i++) {
             for (int j = 0; j < col; j++) {
                 if (i==j || j==col-1-i) {
@@ -29,4 +32,5 @@ public class ForTester {
             System.out.println();
         }
     }
+
 }
