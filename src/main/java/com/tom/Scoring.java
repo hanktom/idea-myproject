33893237685143d5e543fa2c9ae38c5b3a1885f0
Hank Tom <hanktom@gmail.com>
2021-07-14 21:49:50 +0800

    nested if else, read data from file

--- src/main/java/com/tom/Scoring.java
+++ src/main/java/com/tom/Scoring.java
@@ -1,9 +1,24 @@
 package com.tom;
 
+import java.io.*;
+
 public class Scoring {
     public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("data.txt");
+            BufferedReader br = new BufferedReader(fileReader);
+            String line = br.readLine();
+            while(line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         int english = 90;
-        int math = 90;
+        int math = 70;
         String name = "Eric";
         int average = (english+math)/2;
         String grading = "F";
